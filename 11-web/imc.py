bfc409d6fd0464aa61f3051568e0c17ddebc7def
oliverfabrica91 <oliverfabrica91@gmail.com>
2026-08-08 16:57:07 -0300

    minha primeira pagina web

---
+++ 11-web/imc.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.write("Olá, mundo!!! ")
+st.write('ola, meu nome é Oliver da Silva Santos')
+st.write('---sistema de calculos de IMC---')
+st.text_input('Digite seu peso: ')
+st.text_input('Digite sua altura: ')
